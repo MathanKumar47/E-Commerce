52f6578be4f689455877ecf60d59f352d599fcef
MathanKumar47 <mkmathanvj@gmail.com>
2023-08-04 17:25:08 +0530

    04-08-23

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use App\Http\Controllers\WishlistController;
 */
 //Frontend Route
 Route::get('/',[HomeController::class,'index']);
-Route::get('/view-details{id}',[HomeController::class,'view_details']);
+Route::get('/view-details{id}',[HomeController::class, 'view_details']);
 Route::get('/product_by_cat{id}',[HomeController::class,'product_by_cat']);
 Route::get('/product-by-subcat{id}',[HomeController::class,'product_by_subcat']);
 Route::get('/search',[HomeController::class,'search']);
@@ -52,40 +52,40 @@ Route::get('/welcome',[UserController::class,'index'])->name('frontend.welcome')
 
 
 // //Category
-// Route::get('/categories/create',[CategoryController::class,'create']);
-// Route::post('/categories/create',[CategoryController::class,'store'])->name('add.category');
-// Route::get('/edit{category}',[CategoryController::class,'edit']);
-// Route::put('/update{category}',[CategoryController::class,'update']);
-// Route::get('/categories',[CategoryController::class,'index']);
-// Route::delete('/delete{category}',[CategoryController::class,'destroy']);
-// Route::get('/cat-status{category}',[CategoryController::class,'change_status']);
+Route::get('/categories/create',[CategoryController::class,'create']);
+Route::post('/categories/create',[CategoryController::class,'store'])->name('add.category');
+Route::get('/edit{category}',[CategoryController::class,'edit']);
+Route::put('/update{category}',[CategoryController::class,'update']);
+Route::get('/categories',[CategoryController::class,'index']);
+Route::delete('/delete{category}',[CategoryController::class,'destroy']);
+Route::get('/cat-status{category}',[CategoryController::class,'change_status']);
 
-// //SubCategory
-// Route::get('/sub-categories/create',[SubCategoryController::class,'create']);
-// Route::post('/sub-categories/create',[SubCategoryController::class,'store'])->name('add.subcategory');
-// Route::get('/subedit{subCategory}',[SubCategoryController::class,'edit']);
-// Route::put('/subupdate{subCategory}',[SubCategoryController::class,'update']);
-// Route::get('/sub-categories',[SubCategoryController::class,'index']);
-// Route::delete('/subdelete{subCategory}',[SubCategoryController::class,'destroy']);
-// Route::get('/subcat-status{subcategory}',[SubCategoryController::class,'change_status']);
+//SubCategory
+Route::get('/sub-categories/create',[SubCategoryController::class,'create']);
+Route::post('/sub-categories/create',[SubCategoryController::class,'store'])->name('add.subcategory');
+Route::get('/subedit{subCategory}',[SubCategoryController::class,'edit']);
+Route::put('/subupdate{subCategory}',[SubCategoryController::class,'update']);
+Route::get('/sub-categories',[SubCategoryController::class,'index']);
+Route::delete('/subdelete{subCategory}',[SubCategoryController::class,'destroy']);
+Route::get('/subcat-status{subcategory}',[SubCategoryController::class,'change_status']);
 
-// //Brand
-// Route::get('/brands/create',[BrandController::class,'create']);
-// Route::post('/brands/create',[BrandController::class,'store'])->name('add.brand');
-// Route::get('/brandedit{brand}',[BrandController::class,'edit']);
-// Route::put('/brandupdate{brand}',[BrandController::class,'update']);
-// Route::get('/brands',[BrandController::class,'index']);
-// Route::delete('/branddelete{brand}',[BrandController::class,'destroy']);
-// Route::get('/brand-status{brand}',[BrandController::class,'change_status']);
+//Brand
+Route::get('/brands/create',[BrandController::class,'create']);
+Route::post('/brands/create',[BrandController::class,'store'])->name('add.brand');
+Route::get('/brandedit{brand}',[BrandController::class,'edit']);
+Route::put('/brandupdate{brand}',[BrandController::class,'update']);
+Route::get('/brands',[BrandController::class,'index']);
+Route::delete('/branddelete{brand}',[BrandController::class,'destroy']);
+Route::get('/brand-status{brand}',[BrandController::class,'change_status']);
 
-// //Product
-// Route::get('/products/create',[ProductController::class,'create']);
-// Route::post('/products/create',[ProductController::class,'store'])->name('add.product');
-// Route::get('/productedit{product}',[ProductController::class,'edit']);
-// Route::put('/productupdate{product}',[ProductController::class,'update']);
-// Route::get('/products',[ProductController::class,'index']);
-// Route::delete('/productdelete{product}',[ProductController::class,'destroy']);
-// Route::get('/product-status{product}',[ProductController::class,'change_status']);
+//Product
+Route::get('/products/create',[ProductController::class,'create']);
+Route::post('/products/create',[ProductController::class,'store'])->name('add.product');
+Route::get('/productedit{product}',[ProductController::class,'edit']);
+Route::put('/productupdate{product}',[ProductController::class,'update']);
+Route::get('/products',[ProductController::class,'index']);
+Route::delete('/productdelete{product}',[ProductController::class,'destroy']);
+Route::get('/product-status{product}',[ProductController::class,'change_status']);
 
 //ADD-To-CART
 Route::post('/add-to-cart',[CartController::class,'add_to_cart']);
